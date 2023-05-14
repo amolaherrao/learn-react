bad83fb04cb325c15e09689a5e46f800d35c4633
amolaher92 <amolaher92@gmail.com>
2023-05-14 14:19:15 +0530

    added jwttoken helper

---
+++ server/helpers/jwtToken.js
@@ -0,0 +1,26 @@
+import Jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.SECRET_KEY || "amolaherrao";
+
+const jwtToken = {
+  createToken(user) {
+    try {
+      const token = Jwt.sign(user, SECRET_KEY, { expiresIn: "1h" });
+      return token;
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
+
+  verifyToken(token) {
+    try {
+      const splitToken = token.split(" ")[1];
+      const userToken = Jwt.verify(splitToken, SECRET_KEY);
+      return userToken;
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
+};
+
+export default jwtToken;
